b23a3e05da4f7279bf5442d91f4214597452791a
olyalevitskaya <tenshi-desu@mail.ru>
2013-01-22 23:17:33 +0600

    логика изменена в синхронизируемых потоках

--- 4/prog.c
+++ 4/prog.c
@@ -3,16 +3,16 @@
 #include <pthread.h>
 
 
-int rez1 = 0;
-int rez2 = 0;
+int rez = 0;
 int mutex = 0;
 int numofthread = 2;
 
-void* function(void* argument)
+void* functionInc(void* argument)
 {
     __asm("pusha");
     for (int i = 0; i <= 500; ++i)
     {
+	__asm("pusha");
     __asm("xorl %ecx, %ecx");
     __asm("incl %ecx");
     __asm("wait:");
@@ -20,25 +20,46 @@ void* function(void* argument)
     __asm("xorl %eax,%eax");
     __asm("lock; cmpxchgl %ecx, (%ebx)");
     __asm("jnz wait");
-    rez1 = rez1 + 1;
+	__asm("popa");
+    rez = rez + 1;
+	__asm("pusha");
     __asm("movl $0, (%0)" : : "r" (&mutex));
     }
     __asm("popa");
 }
 
+void* functionDec(void* argument)
+{
+    __asm("pusha");
+    for (int i = 0; i <= 500; ++i)
+    {
+	__asm("pusha");
+    __asm("xorl %ecx, %ecx");
+    __asm("incl %ecx");
+    __asm("wait:");
+    __asm("mov %0, %%ebx" : : "r" (&mutex));
+    __asm("xorl %eax,%eax");
+    __asm("lock; cmpxchgl %ecx, (%ebx)");
+    __asm("jnz wait");
+	__asm("popa");
+    rez = rez - 1;
+	__asm("pusha");
+    __asm("movl $0, (%0)" : : "r" (&mutex));
+    }
+    __asm("popa");
+}
+
+
 int main()
 {
     pthread_t thread[numofthread];
     
-    rez1 = 0;
-	rez2 = 0;
-    pthread_create(&thread[0], NULL, function, NULL);
-    pthread_create(&thread[1], NULL, function, NULL);
+    pthread_create(&thread[0], NULL, functionInc, NULL);
+    pthread_create(&thread[1], NULL, functionDec, NULL);
        
     pthread_join(thread[0], NULL);
 	pthread_join(thread[1], NULL);
     
-	rez2 = 500 * numofthread;	
-    if (rez1 < rez2){ printf("Something wrong\n");}
-	else {printf("Threads Synchronized!\n");}
+	if (rez1 != 0){ printf("Something wrong\n"); exit(-1)}
+	printf("Threads Synchronized!\n");
 }
