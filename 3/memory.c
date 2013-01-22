fd297a94262d753494e3f427c7ac65d8eb94aa1c
olyalevitskaya <tenshi-desu@mail.ru>
2013-01-22 23:31:46 +0600

    добавила Tab и переводы строк для читабельности =)

--- 3/memory.c
+++ 3/memory.c
@@ -16,22 +16,41 @@ efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *systab )
 	long long int memtouse=0;	
 	
 	err=uefi_call_wrapper(systab->BootServices->GetMemoryMap,5,&memorymapsize, memorymap, &mapkey, &descriptorsize, &descriptorversion);  
-	if(err!=EFI_BUFFER_TOO_SMALL){Print(L"Error in getting memory map\n");return EFI_SUCCESS;}
+	if(err!=EFI_BUFFER_TOO_SMALL)
+		{
+		Print(L"Error in getting memory map\n");
+		return EFI_SUCCESS;
+		}
 
 	err=uefi_call_wrapper(systab->BootServices->AllocatePool,3, EfiLoaderData,memorymapsize, ((void*)&memorymap));  
-	if(err!=EFI_SUCCESS){Print(L"Error in allocating memory\n");return EFI_SUCCESS;}
+	if(err!=EFI_SUCCESS)
+		{
+		Print(L"Error in allocating memory\n");
+		return EFI_SUCCESS;
+		}
 
 	err=uefi_call_wrapper(systab->BootServices->GetMemoryMap, 5, &memorymapsize, memorymap, &mapkey, &descriptorsize, &descriptorversion);  
-	if(err!=EFI_SUCCESS){Print(L"Error in getting memory map\n");return EFI_SUCCESS;}
+	
+	if(err!=EFI_SUCCESS)
+		{
+		Print(L"Error in getting memory map\n");
+		return EFI_SUCCESS;
+		}
 
 	z=memorymapsize/(sizeof(EFI_MEMORY_DESCRIPTOR));	
 	for(i=0;i<z;i++){
-		if((memorymap[i].Type == EfiBootServicesCode)|| (memorymap[i].Type ==EfiBootServicesData) ||(memorymap[i].Type ==EfiConventionalMemory)){memtouse+=memorymap[i].NumberOfPages;}
+		if((memorymap[i].Type==EfiBootServicesCode)|| (memorymap[i].Type==EfiBootServicesData) ||(memorymap[i].Type==EfiConventionalMemory))
+		{memtouse+=memorymap[i].NumberOfPages;}
 	}
 	memtouse=memtouse*4096;
 
 	err=uefi_call_wrapper(systab->BootServices->FreePool, 1, ((void*)memorymap));
-	if(err!=EFI_SUCCESS){Print(L"Error in free pool fuction\n");return err;}
+	
+	if(err!=EFI_SUCCESS)
+		{
+		Print(L"Error in free pool fuction\n");
+		return err;
+		}
 
 
 	Print(L"%d bytes of memory is avaliable for general use\n",memtouse);
